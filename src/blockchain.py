dade43bedf2e24674187ecfb29a3dd9dcf929ebd
Shamus Clifford <shamusclifford@bpsma.org>
2019-05-24 18:34:49 -0400

    Add Blockchain Class

---
+++ src/blockchain.py
@@ -0,0 +1,28 @@
+import block as block_class
+from hashlib import sha256
+import json
+import time
+class blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.height = -1
+        self.initialize_chain()
+
+    def initialize_chain(self):
+        if self.height is -1:
+            block = block_class.block({'data': 'Genesis Block'})
+            self.add_block(block)
+        
+    def add_block(self, block):
+        block.height = self.height + 1
+        block.time = int(time.time())
+        if len(self.chain) > 0:
+            block.previousBlockHash = self.chain[self.height].hash
+        block.hash = sha256(json.dumps(block.__dict__).encode()).hexdigest()
+        self.chain.append(block)
+        self.height += 1
+
+        if self.chain[self.height] is block:
+            return block
+        return "Error. Block was not added"
